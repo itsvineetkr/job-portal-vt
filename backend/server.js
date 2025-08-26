dcc14e79ebe10c11ef032c6ed295ac3db6c6299c
Vineet Kumar <itsvineetkr@gmail.com>
2025-08-26 23:51:23 +0530

    latest

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 
 // MongoDB
 mongoose
-  .connect("mongodb://localhost:27017/jobPortal", {
+  .connect("mongodb://mongo:27017/jobPortal", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
